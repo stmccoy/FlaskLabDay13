71f2a1adb210fa9dad9f073e81a12fa801d20196
GiulianoDerosas <giulianoderosas@gmail.com>
2021-04-14 18:50:35 +0400

    Order list updated with hyperlinks

--- controllers/controller.py
+++ controllers/controller.py
@@ -4,4 +4,9 @@ from models.order_list import orders
 
 @app.route('/')
 def index():
-    return hello world
+    return render_template('index.html', title='Home', orders=orders, length=len(orders))
+
+
+@app.route('/orders/<index>/')
+def order(index):
+    return render_template('order.html', title='Home', orders=orders, index=int(index))
